31d9f23581d4b91d15a131a76438ddcdc6c67a0c
Harvey Burgos <hburgos@blastasia.com>
2022-08-04 21:08:49 +0800

    Change API

--- src/app/hero-service.ts
+++ src/app/hero-service.ts
@@ -12,25 +12,25 @@ export class HeroService {
     this.http.options
   }
 
-  // getHeroList() {
-  //   return this.http.get('https://api.jsonbin.io/v3/b/62e79f5460c3536f3fcc853f', {
-  //     headers: { 'X-Master-Key': '$2b$10$DapXgB9/4AcoVGhstD7y7.rqazG.CYEdxt0pHHEg4rN0E9uJ83zvi' }
-  //   }).subscribe((result: any) => {
-  //     this.heroList = result.record.data;
-  //     this.heroList.sort((a: any, b: any) => this.compareName(a,b));
-  //   });
-  // }
-
   getHeroList() {
-    return this.http.get<HeroDTO>('../assets/hero-list.json').subscribe((result: any) => {
-      this.heroList = result.data;
-      this.originalHeroList = result.data;
-      this.heroList.forEach(hero => {
-        hero.isSelected = false;
-      });
+    return this.http.get('https://api.jsonbin.io/v3/b/62e79f5460c3536f3fcc853f', {
+      headers: { 'X-Master-Key': '$2b$10$DapXgB9/4AcoVGhstD7y7.rqazG.CYEdxt0pHHEg4rN0E9uJ83zvi' }
+    }).subscribe((result: any) => {
+      this.heroList = result.record.data;
       this.heroList.sort((a: any, b: any) => this.compareName(a,b));
     });
   }
+
+  // getHeroList() {
+  //   return this.http.get<HeroDTO>('../assets/hero-list.json').subscribe((result: any) => {
+  //     this.heroList = result.data;
+  //     this.originalHeroList = result.data;
+  //     this.heroList.forEach(hero => {
+  //       hero.isSelected = false;
+  //     });
+  //     this.heroList.sort((a: any, b: any) => this.compareName(a,b));
+  //   });
+  // }
 
   compareName(a: any, b: any) {
     // converting to uppercase to have case-insensitive comparison
